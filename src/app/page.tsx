ed3ef4ef7babc87ea769cbc60b315c41f3ff5464
Astha <ashutosh1102.edu.in>
2024-12-28 18:14:58 +0530

    Added Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
    <>
-   hello
+    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
+     <Navbar/>
+    </div>
    </>
   );
 }
