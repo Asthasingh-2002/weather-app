4a76a16256596a68cd436019c6c4806a0cac6acb
Astha <ashutosh1102.edu.in>
2024-12-29 01:42:11 +0530

    Integrated openweather api to fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,109 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Navbar from "@/components/Navbar";
 
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
+
 export default function Home() {
+  const [weather, setWeather] = useState<WeatherData[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?q=pune&cnt=56`,
+          {
+            params: {
+              q: 'pune',
+              appid: process.env.NEXT_PUBLIC_WEATHER_KEY,
+            },
+          }
+        );
+        setWeather(response.data.list);
+      } catch (err: any) {
+        setError(err.message || 'An error occurred');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log("checking", weather);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  if (loading)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
+  if (error)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        {/* @ts-ignore */}
+        <p className="text-red-400">{error.message}</p>
+      </div>
+    );
   return (
    <>
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
