5feb0f65b52cda10a1fddc4373a1f97bc03e74a4
Astha <ashutosh1102.edu.in>
2024-12-29 13:25:19 +0530

    Add weather description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ import Container from '@/components/Container';
 import { convertKelvinToCelsius } from '@/utils/convertKelvinToCelsius';
 import WeatherIcon from '@/components/WeatherIcon';
 import { getDayOrNightIcon } from '@/utils/getDayOrNightIcon';
+import WeatherDetails from '@/components/WeatherDetails';
+import { metersToKilometers } from '@/utils/metersToKilometers';
+import { convertWindSpeed } from '@/utils/convertWindSpeed';
 // import WeatherDetails from "@/components/WeatherDetails";
 // import WeatherIcon from "@/components/WeatherIcon";
 
@@ -167,19 +170,48 @@ console.log("checking",weatherData)
                           {format(parseISO(d.dt_txt), "h:mm a")}
                         </p>
 
-                        <WeatherIcon iconName={d.weather[0].icon} />
-                        {/* <WeatherIcon
+                        {/* <WeatherIcon iconName={d.weather[0].icon} /> */}
+                        <WeatherIcon
                           iconName={getDayOrNightIcon(
                             d.weather[0].icon,
                             d.dt_txt
                           )}
-                        /> */}
-                        {/* <p>{convertKelvinToCelsius(d?.main.temp ?? 0)}°</p> */}
+                        />
+                        <p>{convertKelvinToCelsius(d?.main.temp ?? 0)}°</p>
                       </div>
                     ))}
                   </div>
                 </Container>
           </div>
+          <div className=" flex gap-4">
+                {/* left  */}
+                <Container className="w-fit  justify-center flex-col px-4 items-center ">
+                  <p className=" capitalize text-center">
+                    {firstData?.weather[0].description}{" "}
+                  </p>
+                  <WeatherIcon
+                    iconName={getDayOrNightIcon(
+                      firstData?.weather[0].icon ?? "",
+                      firstData?.dt_txt ?? ""
+                    )}
+                  />
+                </Container>
+                 {/* right  */}
+                <Container className="bg-yellow-300/80  px-6 gap-4 justify-between overflow-x-auto">
+                  <WeatherDetails
+                    visability={metersToKilometers(
+                      firstData?.visibility ?? 10000
+                    )}
+                    airPressure={`${firstData?.main.pressure} hPa`}
+                    humidity={`${firstData?.main.humidity}%`}
+                    sunrise={format(weatherData?.city.sunrise ?? 1702949452, "H:mm")}
+                    // sunrise={}
+                    sunset={format(weatherData?.city.sunset ?? 1702517657, "H:mm")}
+                    windSpeed={convertWindSpeed(firstData?.wind.speed ?? 1.64)}
+                  />
+                </Container>
+               
+              </div>
         </section>
         {/* 7-day forecast data */}
         <section></section>
